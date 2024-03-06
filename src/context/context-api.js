ab116e373fbeeb4670f2a3dda0274f73394f4d2e
Furkan Kalkan <furkankalkan502@gmail.com>
2024-03-06 14:19:18 +0300

    context api refactoring v1

--- src/context/context-api.js
+++ src/context/context-api.js
@@ -1,1 +1,66 @@
-// Context Api'ye çevirme işlemi bu branchde sağlanacak.
+import { createContext, useState, useEffect } from "react";
+
+const TodoContext = createContext()
+
+function Provider({children}) {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        fetchTasks()
+    },[])
+
+    const fetchTasks = async() => {
+        const response = await axios.get('http://localhost:3001/tasks')
+        setTasks(response.data)
+    }
+
+    const handleSubmit = async(task) => {
+        if(task.taskText !== ''){
+            const response = await axios.post('http://localhost:3001/tasks', {
+                taskText:task.taskText,
+                taskLevel:task.taskLevel,
+                isDone:task.isDone
+            })
+            const updatedTasks = [...tasks, response.data]
+            setTasks(updatedTasks)
+        } 
+        else window.alert('Please add task!')  
+    }
+
+    const handleEdit = (id, newText) => {
+        const updatedTasks = tasks.map(task => {
+            if(task.id === id){
+                return {...task, taskText:newText}
+            }
+            return task
+        })
+        setTasks(updatedTasks)
+    }
+
+    const handleDone = async(id, done) => {
+        const response = await axios.patch(`http://localhost:3001/tasks/${id}`,{
+           isDone:done
+        })
+
+        const updatedTasks = tasks.map(task => {
+            if(task.id === id){
+                return {...task, ...response.data}
+            //    return task.isDone ? {...task, isDone:false} : {...task, isDone:true}
+                
+            }
+            return task
+        })
+        setTasks(updatedTasks)
+        console.log(tasks)
+    }
+    const handleDelete = async(id) => {
+        await axios.delete(`http://localhost:3001/tasks/${id}`)
+        const updatedTasks = tasks.filter(task => task.id !== id) 
+        setTasks(updatedTasks)
+    }
+    
+}
+
+
+export {Provider}
+export default TodoContext
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from './context/context-api';
 
 const el = document.getElementById('root')
 
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />)
+root.render(
+    <Provider>
+     <App />
+    </Provider>
+)
